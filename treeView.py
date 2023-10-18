554dd53dc990856c5c683680f4956dbda4191bd2
Qvanbit <105719080+Qvanbit@users.noreply.github.com>
2023-10-19 01:20:01 +0300

    Update treeView.py
    test

--- treeView.py
+++ treeView.py
@@ -1,59 +1,66 @@
 import os
+import re
 
-from PyQt6.QtWidgets import QTreeView, QVBoxLayout, QWidget, QLineEdit, QMainWindow
+from PyQt6.QtWidgets import QTreeView, QVBoxLayout, QWidget, QLineEdit, QMainWindow, QApplication
 from PyQt6.QtGui import QStandardItem, QStandardItemModel
-from PyQt6.QtCore import QSortFilterProxyModel, QDir
+from PyQt6.QtCore import QDir, Qt, QSortFilterProxyModel, QRegularExpression
+import concurrent.futures
+
 
 class FileSystemTreeViewer(QMainWindow):
     def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs) 
+        super().__init__(*args, **kwargs)
         self.setWindowTitle("Отображение дерева файловой системы")
         self.setMaximumSize(1200, 1000)
         self.setMinimumSize(800, 600)
-        # Главный виджет
+        self.app = QApplication.instance()
+
         self.widget = QWidget(self)
         self.setCentralWidget(self.widget)
         self.layout = QVBoxLayout(self.widget)
-        # Модель данных
+
         self.model = QStandardItemModel()
         self.model.setHorizontalHeaderLabels(["Файловая система"])
-        # Модель сортировки
+
         self.sortModel = QSortFilterProxyModel()
         self.sortModel.setSourceModel(self.model)
         self.sortModel.setFilterKeyColumn(0)
-        
-        # Отображение дерева
+
         self.treeView = QTreeView(self)
         self.treeView.setModel(self.sortModel)
         self.treeView.setSortingEnabled(True)
         self.treeView.setEditTriggers(QTreeView.EditTrigger.NoEditTriggers)
-        # Виджет для фильтрации
+
         self.filterLineEdit = QLineEdit(self)
         self.filterLineEdit.textChanged.connect(self.filter_tree)
-        
+
         self.layout.addWidget(self.filterLineEdit)
         self.layout.addWidget(self.treeView)
-        
-        self.populate_tree()
 
-    def populate_tree(self):
-        rootPath = os.path.expanduser("~")
+        self.load_tree(os.path.expanduser("~"))
+        rootIndex = self.sortModel.mapFromSource(self.model.index(0, 0))
+        self.treeView.setExpanded(rootIndex, True)
+
+    def load_tree(self, rootPath):
+        self.cache = {}
         dir = QDir(rootPath)
-        self.add_items(self.model.invisibleRootItem(), dir)
+        root_item = self.model.invisibleRootItem()
+        item = QStandardItem(dir.dirName())
+        item.setData(rootPath, Qt.ItemDataRole.UserRole)
+        root_item.appendRow(item)
+        self.cache[rootPath] = item
+        self.add_items(item, dir)
 
     def add_items(self, parent_item, dir):
-        for entry_info in dir.entryInfoList(QDir.Filter.AllEntries | QDir.Filter.NoDotAndDotDot, QDir.SortFlag.Name):
+        for entry_info in dir.entryInfoList(QDir.Filter.AllEntries | QDir.Filter.Hidden | QDir.Filter.NoDotAndDotDot,
+                                            QDir.SortFlag.Name):
             item = QStandardItem(entry_info.fileName())
-            parent_item.appendRow(item)
+            item.setData(entry_info.filePath(), Qt.ItemDataRole.UserRole)
             if entry_info.isDir():
+                self.cache[entry_info.filePath()] = item
                 self.add_items(item, QDir(entry_info.filePath()))
+            parent_item.appendRow(item)
 
     def filter_tree(self):
         text = self.filterLineEdit.text()
         self.sortModel.setFilterFixedString(text)
-
-
-
-
-
-
